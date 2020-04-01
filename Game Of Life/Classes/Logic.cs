cacae112000d54e62a86b9e220bbef0bf31d26a1
Chase <chase@chaseburr.com>
2020-03-31 21:13:31 -0600

    Updated logic, file names, and removed a couple unused files.

--- Game Of Life/Classes/Logic.cs
+++ Game Of Life/Classes/Logic.cs
@@ -7,60 +7,40 @@ namespace Game_Of_Life.Classes
 {
     public class Logic
     {
-        // private bool StartStop = false;
-
         private Cells[,] CurrentGeneration = new Cells[22, 22];
         private Cells[,] NextGeneration = new Cells[22, 22];
 
-        // Visualization
-        // { 0 c 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { a x a 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 c 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 }
-
-
-        // { 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 },
-        // { 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 },
-
         public Logic(List<Cells> cells)
         {
-            foreach(Cells cell in cells)
-            {
-                CurrentGeneration[cell.x + 1, cell.y + 1] = cell;
-            }
+            foreach (Cells cell in cells)
+                CurrentGeneration[cell.y + 1, cell.x + 1] = cell;
+            doLogic();
         }
 
-        private void doogic()
+        private void doLogic()
         {
             for (int i = 1; i < 19; i++)
             {
                 for (int j = 1; j < 20; j++)
                 {
                     List<Cells> neighbors = new List<Cells>();
-                    for(int k = -1; k < 2; k++)
+                    for (int k = -1; k < 2; k++)
                     {
-                        for(int l = -1; l < 2; l++)
+                        for (int l = -1; l < 2; l++)
                         {
-                            if(k != 0 && l != 0)
+                            if (k != 0 && l != 0)
                             {
-                                if (CurrentGeneration[i + k, j + l] != null) neighbors.Add(CurrentGeneration[i + k, j + l]);
+                                if (CurrentGeneration[i + k, j + l] != null)
+                                    neighbors.Add(CurrentGeneration[i + k, j + l]);
                             }
                         }
                     }
-                    if(neighbors.Count > 2)
+                    if (neighbors.Count > 2)
                     {
                         int red = 0;
                         int green = 0;
                         int blue = 0;
-                        foreach(Cells cell in neighbors)
+                        foreach (Cells cell in neighbors)
                         {
                             red += cell.red;
                             green += cell.green;
@@ -69,31 +49,6 @@ namespace Game_Of_Life.Classes
                         NextGeneration[i, j] = new Cells(i, j);
                         NextGeneration[i, j].setColor(new int[] { red, green, blue });
                     }
-                    /*if (CurrentGeneration[i, j] != null)
-                    {
-                        // Check upper and lower
-                        if (CurrentGeneration[i - 1, j] != null && CurrentGeneration[i + 1, j] != null)
-                        {
-                            NextGeneration[i, j + 1] = new Cells(i, j);
-                            // NextGeneration[i, j + 1].setColor();
-                            // NextGeneration[i, j - 1] = 1;
-                        } else
-                        {
-                            *//*NextGeneration[i, j] = 0;
-                            NextGeneration[i, j] = 0;*//*
-                        }
-
-                        *//*// Check Sides
-                        if (CurrentGeneration[i, j - 1] == 1 && CurrentGeneration[i, j + 1] == 1)
-                        {
-                            NextGeneration[i, j + 1] = 1;
-                            NextGeneration[i, j - 1] = 1;
-                        } else
-                        {
-                            NextGeneration[i, j] = 0;
-                            NextGeneration[i, j] = 0;
-                        }*//*
-                    }*/
                 }
             }
         }
@@ -101,12 +56,13 @@ namespace Game_Of_Life.Classes
         public List<Cells> getList()
         {
             List<Cells> cells = new List<Cells>();
+
             // logic to return list of cells
-            for(int i = 0; i < NextGeneration.Length; i++)
+            for (int i = 1; i < 22; i++)
             {
-                for(int j = 0; j < NextGeneration.Length; j++)
+                for (int j = 1; j < 22; j++)
                 {
-                    if(NextGeneration[i,j] != null)
+                    if (NextGeneration[i, j] != null)
                     {
                         cells.Add(NextGeneration[i, j]);
                     }
--- Game Of Life/wwwroot/js/gameoflife.js
+++ Game Of Life/wwwroot/js/gameoflife.js
@@ -1,47 +1,106 @@
-﻿//grid width and height
-let bw = bh = 800;
-let canvas, ctx;
+﻿"use strict";
 
+// Create connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/gameHub").build();
 
-window.onload = function () {
+// Start connection
+connection.start().then(function () {
+    console.log("connected");
+});
+
+// Variables
+let canvas, ctx, cellSize;
+let cellDensity = 20;
+let canvasSize = 800;
+let cells = [];
+
+window.addEventListener('load', function () {
     canvas = document.getElementById('board');
-    canvas.width = canvas.height = 801;
+    canvas.width = canvas.height = canvasSize;
+    canvas.addEventListener('mousedown', function (e) {
+        canvasClick(canvas, e);
+    });
 
     ctx = canvas.getContext('2d');
-    ctx.strokeStyle = 'black';
 
-    canvas.addEventListener('mousedown', onDown, false);
+    cellSize = Math.floor(canvas.width / cellDensity);
+    drawGrid();
 
-    drawBoard();
+    // control events
+    document.getElementById('timeIntervalSlider').addEventListener('change', (e) => connection.invoke('AutoGenerationInterval', parseInt(e.target.value)));
+    document.getElementById('pausePlayButton').addEventListener('click', (e) => connection.invoke('AutoGenerationToggle'));
+    document.getElementById('iterateButton').addEventListener('mousedown', () => connection.invoke('NextGeneration'));
+});
 
+/* Client Display Start */
+function drawGrid() {
+    // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-}
-
-// Grabs X and Y cords
-function onDown(event) {
-    let cx = event.pageX;
-    let cy = event.pageY;
-
-    alert("X =" + cx + ' Y: ' + cy);
-}
-
-
-function drawBoard() {
     // Draw Vertical lines
-    for (let x = 0; x <= bw; x += 40) {
+    for (let x = 0; x <= canvasSize; x += cellSize) {
         ctx.moveTo(x, 0);
-        ctx.lineTo(x, bh);
+        ctx.lineTo(x, canvasSize);
     }
 
     // Draw Horizontal lines
-    for (let x = 0; x <= bh; x += 40) {
+    for (let x = 0; x <= canvasSize; x += cellSize) {
         ctx.moveTo(0, x);
-        ctx.lineTo(bw, x);
+        ctx.lineTo(canvasSize, x);
     }
 
     // Set color and display lines
     ctx.strokeStyle = "black";
     ctx.stroke();
+
+    // Draws tiles
+    for (let i = 0; i < cells.length; i++) cells[i].draw(ctx);
 }
 
+class cell {
+    constructor(x, y, red, green, blue) {
+        var _this = this;
+        (function () {
+            _this.x = x || null;
+            _this.y = y || null;
+            _this.red = red || 0;
+            _this.green = green || 0;
+            _this.blue = blue || 0;
+        })();
 
+        this.draw = function (ctx) {
+            if (typeof _this.x == 'undefined' || typeof _this.y == 'undefined') {
+                console.log('Error of the undefined...');
+                return;
+            }
+            let cellBorder = 4;
+            ctx.beginPath();
+            ctx.fillStyle = "rgb(" + _this.red + ', ' + _this.green + ', ' + _this.blue + ")";
+            ctx.fillRect((_this.x * cellSize) + cellBorder, (_this.y * cellSize) + cellBorder, cellSize - (cellBorder * 2), cellSize - (cellBorder * 2));
+        };
+    }
+}
+/* Client Display End */
+
+
+/* Server Interactions Start */
+connection.on("UpdateCells", (cellsFromServer) => {
+    // empty and fill cell array
+    cells = [];
+    cellsFromServer.forEach(element => cells.push(new cell(element.x, element.y, element.red, element.green, element.blue)));
+    console.log("updating board");
+    drawGrid();
+});
+connection.on("ChangeIntervalSlider", (interval) => document.getElementById("timeIntervalSlider").value = interval);
+connection.on("ChangePlayStopButton", (playStop) => document.getElementById("pausePlayButton").value = (playStop ? "Stop" : "Play"));
+
+function canvasClick(canvas, event) {
+    // get click position within canvas element
+    let rect = canvas.getBoundingClientRect();
+    let x = Math.floor((event.clientX - rect.left) / cellSize);
+    let y = Math.floor((event.clientY - rect.top) / cellSize);
+
+    // Sends updated tile server side
+    connection.invoke("SendNewCells", x, y);
+}
+/* Server Interections End */
